ce28d25da2003b976bab26a90b5e7a9679e21371
DouglasGusmao <ruggery402@gmail.com>
2017-08-14 11:05:50 -0300

    Correções no cadastrar visita e no pesquisar

--- geral/visita/buscar_visita.php
+++ geral/visita/buscar_visita.php
@@ -1,19 +1,44 @@
- <?php
+<?php
     include '../../conexao/conexao.php'; /* include da conexão com o baco de dados */
-    
-    if (isset($_POST["nome"])){
+	
+	function inverteData1($data1){
+     if(count(explode("/",$data1)) > 1){
+         return implode("-",array_reverse(explode("/",$data1)));
+     }elseif(count(explode("-",$data1)) > 1){
+         return implode("/",array_reverse(explode("-",$data1)));
+     }
+ }
+
+	function inverteData2($data2){
+     if(count(explode("/",$data2)) > 1){
+         return implode("-",array_reverse(explode("/",$data2)));
+     }elseif(count(explode("-",$data2)) > 1){
+         return implode("/",array_reverse(explode("-",$data2)));
+     }
+ }
+
+	if ($_POST["nome"] != null){
       $query = "select visita.codigo,visita.matricula_usuario,usuario.nome,visita.data,visita.hora from visita inner join usuario on visita.matricula_usuario = usuario.matricula and usuario.matricula= visita.matricula_usuario where usuario.nome like ?";
       
       $nome= (isset($_POST["nome"])?$_POST["nome"]:"");
+	  
+      $stmt = $conexao->prepare($query); 
+      $stmt->bindValue(1, "%".$nome."%");
+	  
+	
+	}else if (isset($_POST["data1"])&&($_POST["data2"])){
+      $query = "select visita.codigo,visita.matricula_usuario,usuario.nome,visita.data,visita.hora from visita inner join usuario on visita.matricula_usuario = usuario.matricula and usuario.matricula= visita.matricula_usuario where visita.data between ? and ? ";
+      
+      $data1= (isset($_POST["data1"])?$_POST["data1"]:"");
+	  $data2= (isset($_POST["data2"])?$_POST["data2"]:"");
       
       $stmt = $conexao->prepare($query); 
-      $stmt->bindValue(1,"%" .$nome ."%");
+      $stmt->bindValue(1, inverteData2($data1));
+	  $stmt->bindValue(2, inverteData2($data2));
 
     }else{
-
      $stmt = $conexao->query ("select visita.codigo,visita.matricula_usuario,usuario.nome,visita.data,visita.hora from visita inner join usuario on visita.matricula_usuario = usuario.matricula and usuario.matricula= visita.matricula_usuario");
     }
-
         echo "
           <div class='all panel panel-success' id='resultBusca'>
             <div class='row panel-heading'>
@@ -36,10 +61,7 @@
                     <img src='geral/img/alterar.png' width='40'>
                 </div>
             </div>";
-
-
         $stmt->execute();
-
         $resultado = $stmt->fetchAll();
         $class="";
         foreach($resultado as $linha){
@@ -63,7 +85,6 @@
                     <a href='#' onclick=\"carregaPagina('geral/visita/cadastrar_visita.php?codigo=".$linha['codigo']."');\">Alterar </a>
                   </div>
                 </div>";
-
                 $class = ($class==""?"active":"");
         }
       echo "</div> 
--- geral/visita/index.php
+++ geral/visita/index.php
@@ -17,11 +17,25 @@
 
         <div class="container">
           <div class="row">
-            <div class="col-md-6"><input type="text" name="nome" id='campoPesquisa' placeholder ="Digite o nome do agente..." class="input" onfocus="this.value=''"/> </div>
+            <div class="col-md-3">
+			<label>Data 01:</label>
+            <input type="text" name="data1" id='campoPesquisa' placeholder ="Data 01" class="input" onfocus="this.value=''"/> 
+			
+            </div>
+			
+            <div class="col-md-3">
+			<label>Agente:</label>
+            <input type="text" name="nome" id='campoPesquisa' placeholder ="Digite o nome do agente" class="input" onfocus="this.value=''"/>
+			
+            </div>
               <div class="col-md-3"> 
                 <button onclick="buscar('geral/visita/buscar_visita.php');" class="btn btn-md btn-success btn-block">Buscar</button>
                 <div class="col-md-3"></div>
               </div>
+				<div class="col-md-6">
+				<label>Data 02:</label>
+            			<input type="text" name="data2" id='campoPesquisa' placeholder ="data2" class="input" onfocus="this.value=''"/>
+            		</div>
            </div>
         </div>
       </form> 
